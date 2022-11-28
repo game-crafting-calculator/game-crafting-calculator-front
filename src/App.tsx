61be2cb5602086185bf42c91d5342f5f21b64fb0
Adrien Gouacide <adrien.gouacide@gmail.com>
2022-11-28 12:23:49 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+
+//ROUTER IMPORTS
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+//PAGES
+import ComponentsViewer from "./pages/components-viewer/components-viewer";
+import Register from "./pages/register/register";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/viewer" element={<ComponentsViewer />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
