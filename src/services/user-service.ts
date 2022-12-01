bf28a914d10b41fa8afb051e5e18f441ef82c835
Adrien Gouacide <adrien.gouacide@gmail.com>
2022-12-01 11:09:10 +0100

    added auth

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,23 +1,47 @@
 import axios from "axios";
 import { API_URL, USER_ROUTE } from "../environment";
 import { User } from "../types";
+import { setToken, getToken } from "./local-storage-service";
 
 const URL = `${API_URL}/${USER_ROUTE}`;
 
-const register = (user: User) => {
-  return axios.post(`${URL}/register`, { user });
+const config = {
+  headers: {
+    authorization: getToken(),
+  },
 };
 
-const login = (email: string, password: string) => {
-  return axios.post(`${URL}/login`, { email, password });
+const register = async (user: User) => {
+  let response = await axios.post(`${URL}/signup`, user, config);
+
+  if (response.status === 201) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-const updateProfile = (user: User) => {
-  return axios.put(`${URL}/update`, { user });
+const login = async (user: User) => {
+  let response = await axios.post(`${URL}/login`, user, config);
+
+  if (response.status === 200) {
+    setToken(response.data.token);
+    return true;
+  } else {
+    return false;
+  }
 };
 
-const deleteAccount = (user: User) => {
-  return axios.delete(`${URL}/delete`);
+const updateProfile = async (user: User) => {
+  return axios.put(`${URL}/update`, user, config);
 };
 
-export default { register, login, updateProfile, deleteAccount };
+const deleteAccount = async (user: User) => {
+  return axios.delete(`${URL}/delete`, config);
+};
+
+//Auth
+const isLoggedIn = () => {
+  return getToken() ? true : false;
+};
+export default { register, login, updateProfile, deleteAccount, isLoggedIn };
