e8b3a031d04d4f52baf9e9b7b50844398e4c0420
Adrien Gouacide <adrien.gouacide@gmail.com>
2023-01-04 11:30:44 +0100

    Pourquoi j'ai pas commit ce foutu fichier avant je n'en peux plus

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://192.168.0.40:3000/api";
+export const API_URL = "https://game-crafting-calculator-back.onrender.com/api";
 export const USER_ROUTE = "/user";
 export const ITEM_ROUTE = "/item";
 export const RECIPE_ROUTE = "/recipe";
