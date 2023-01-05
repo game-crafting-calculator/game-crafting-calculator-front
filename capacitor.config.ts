6699a33c7cd365c0d179af5997f70d1caa32709c
Adrien Gouacide <adrien.gouacide@gmail.com>
2023-01-05 11:51:15 +0100

    added capacitor and updated .gitignore

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gamecraftingcalculator.app',
+  appName: 'Game Crafting Calculator',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
