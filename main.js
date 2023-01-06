a5e635971dd0cfcae73cd12ee7a745a638f0375d
Adrien Gouacide <adrien.gouacide@gmail.com>
2023-01-06 14:12:41 +0100

    electron setup

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadURL("https://game-crafting-calculator-front.onrender.com/");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
