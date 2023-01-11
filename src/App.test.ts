6eec6be072fcfaa9d7574cb768c27c0563a89e02
Adrien Gouacide <adrien.gouacide@gmail.com>
2023-01-11 14:28:06 +0100

    added

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,11 +1,29 @@
-import { describe, it, expect } from "vitest";
+import { missingField } from "./utils/email-validator";
+import { test } from "vitest";
 
-describe("something truthy and falsy", () => {
-  it("true to be true", () => {
-    expect(true).toBe(true);
-  });
+test("should return an empty array if all fields are defined", async () => {
+  const obj = {
+    field1: "value1",
+    field2: "value2",
+  };
+  const missing = missingField(obj);
+  expect(missing).toEqual([]);
+});
 
-  it("false to be false", () => {
-    expect(false).toBe(false);
-  });
+test("should return missing fields if some fields are undefined", async () => {
+  const obj = {
+    field1: undefined,
+    field2: "value2",
+  };
+  const missing = missingField(obj);
+  expect(missing).toEqual(["field1"]);
+});
+
+test("should return all fields if all fields are undefined", async () => {
+  const obj = {
+    field1: undefined,
+    field2: undefined,
+  };
+  const missing = missingField(obj);
+  expect(missing).toEqual(["field1", "field2"]);
 });
